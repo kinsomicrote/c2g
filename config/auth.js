59013b06e90cdd027d7a6f77b442f77e756d5068
kinsomicrote <kinsomicrote.inc@gmail.com>
2017-08-02 14:03:36 +0100

    modified auth

--- config/auth.js
+++ config/auth.js
@@ -1,12 +1,12 @@
 module.exports = {
   facebookAuth: {
-    clientID: '108316649851578',
-    clientSecret: '108316649851578',
+    clientID: 'xxx',
+    clientSecret: 'xxx',
     callbackURL: 'http://localhost:3000/auth/facebook/callback',
   },
   googleAuth: {
-    clientID: '667783449039-8m9v71mq6no985fuqia4q48ksek1k6uo.apps.googleusercontent.com',
-    clientSecret: 'tS2CR3LBcmCIi0ah_ZqSZvue',
+    clientID: 'xxx.apps.googleusercontent.com',
+    clientSecret: 'xxx',
     callbackURL: 'http://localhost:3000/auth/google/callback',
   },
 };
